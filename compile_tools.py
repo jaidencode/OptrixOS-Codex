86a6231c29356c1f1fbd75f5230496e1394db579
jaidencode <33370705+jaidencode@users.noreply.github.com>
2025-06-20 18:43:08 -0400

    Add minimal GRUB-based OS build

--- compile_tools.py
+++ compile_tools.py
@@ -1,3 +1,4 @@
+import os, shutil, subprocess
 # --- CONFIGURATION ---
 TOOLCHAIN_DIR = os.environ.get("TOOLCHAIN_DIR") or r"C:\\Users\\jaide\\Downloads\\i686-elf-tools-windows\\bin"
 CC = os.environ.get("CC") or os.path.join(TOOLCHAIN_DIR, "i686-elf-gcc.exe")
@@ -23,3 +24,26 @@ CDRTOOLS_DIR = os.environ.get("CDRTOOLS_DIR") or r"C:\\Program Files (x86)\\cdrt
 MKISOFS_EXE = os.environ.get("MKISOFS") or os.path.join(CDRTOOLS_DIR, "mkisofs.exe")
 if not os.path.isfile(MKISOFS_EXE):
     MKISOFS_EXE = shutil.which("mkisofs") or "mkisofs"
+
+def run(cmd):
+    print(' '.join(cmd))
+    subprocess.check_call(cmd)
+
+
+def compile_kernel():
+    os.makedirs('build', exist_ok=True)
+    run([NASM, '-f', 'elf32', 'src/boot.s', '-o', 'build/boot.o'])
+    run([CC, '-m32', '-ffreestanding', '-O2', '-c', 'src/kernel.c', '-o', 'build/kernel.o'])
+    run([LD, '-m', 'elf_i386', '-T', 'linker.ld', '-o', 'build/kernel.bin', 'build/boot.o', 'build/kernel.o'])
+
+def make_iso():
+    os.makedirs('build/isofiles/boot/grub', exist_ok=True)
+    shutil.copy('build/kernel.bin', 'build/isofiles/boot/kernel.bin')
+    with open('build/isofiles/boot/grub/grub.cfg', 'w') as f:
+        f.write('set timeout=0\n')
+        f.write('menuentry "OptrixOS" { multiboot /boot/kernel.bin }\n')
+    run(['grub-mkrescue', '-o', 'OptrixOS.iso', 'build/isofiles'])
+
+if __name__ == '__main__':
+    compile_kernel()
+    make_iso()
