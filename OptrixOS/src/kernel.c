a13a516a06ec2962ff0dbe4026a4155bb621ffb2
jaidencode <33370705+jaidencode@users.noreply.github.com>
2025-06-21 01:55:21 -0400

    Implement IDT, ISR and GDT; update build script

--- OptrixOS/src/kernel.c
+++ OptrixOS/src/kernel.c
@@ -1,8 +1,13 @@
 #include "kernel.h"
 #include "io.h"
 #include "shell.h"
+#include "isr.h"
+
+extern void gdt_install();
 
 void kmain() {
+    gdt_install();
+    isr_install();
     clear_screen();
     print("DreamOS Kernel Ready\n");
     shell();
